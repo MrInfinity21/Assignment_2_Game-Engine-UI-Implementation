d2e2e9998c95758c0a5808a92cf737b043baea3e
Nithish Velu <n.kumaraguru540@mybvc.ca>
2025-12-05 00:52:51 -0700

    Added Scripts and Attached it to Components

---
+++ Assets/Scripts/OptionsMenu/OptionMenu.cs
@@ -0,0 +1,89 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionMenu : MonoBehaviour
+{
+    [Header("UI")]
+    public Slider _masterSlider;
+    public Slider _sfxSlider;
+    public Toggle _subtitlesToggle;
+
+    public GameObject _confirmPopup;
+
+    private GameSettings pendingSettings = new GameSettings();
+    private GameSettings currentSettings = new GameSettings();
+
+    private bool hasUnsavedChanges = false;
+
+    private void Start()
+    {
+        LoadSettingsToUI();
+    }
+
+    void LoadSettingsToUI()
+    {
+        _masterSlider.value = currentSettings._masterVolume;
+        _sfxSlider.value = currentSettings._sfxVolume;
+        _subtitlesToggle.isOn = currentSettings._subtitlesOn;
+
+        pendingSettings._masterVolume = currentSettings._masterVolume;
+        pendingSettings._sfxVolume = currentSettings._sfxVolume;
+        pendingSettings._subtitlesOn = currentSettings._subtitlesOn;
+
+    }
+
+    public void OnMasterChanged(float value)
+    {
+        pendingSettings._masterVolume = value;
+        hasUnsavedChanges = true;
+    }
+
+    public void OnSFXChanged(float value)
+    {
+        pendingSettings._sfxVolume = value;
+        hasUnsavedChanges = true;
+    }
+
+    public void OnSubtitlesChanged(bool value)
+    {
+        pendingSettings._subtitlesOn = value;
+        hasUnsavedChanges = true;
+    }
+
+    public void ApplyChanges()
+    {
+        _confirmPopup.SetActive(true);
+    }
+
+    public void ConfirmApplyYes()
+    {
+        currentSettings._masterVolume = pendingSettings._masterVolume;
+        currentSettings._sfxVolume = pendingSettings._sfxVolume;
+        currentSettings._subtitlesOn = pendingSettings._subtitlesOn;
+
+        hasUnsavedChanges = false;
+        _confirmPopup.SetActive(false);
+    }
+
+    public void ConfirmApplyNo()
+    {
+        _confirmPopup.SetActive(false);
+    }
+
+    public void OnBackPressed()
+    {
+        if (hasUnsavedChanges)
+        {
+            _confirmPopup.SetActive(true);
+        }
+        else
+        {
+            ReturnToPauseMenu();
+        }
+    }
+
+    private void ReturnToPauseMenu()
+    {
+        Debug.Log("Loading previous scene.");
+    }
+}
